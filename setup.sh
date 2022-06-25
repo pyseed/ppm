7930abf32a62a01ca60cbd3e7a021d531e921519
pyseed <pyseed.dev@gmail.com>
2022-06-25 10:49:02 +0200

    [ADD] setup dev env

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
 ppmd=$(dirname -- $(readlink -f -- "$0"))
-
 ln -sf ${ppmd}/ppm ~/.local/bin/ppm
+
+mode=${1:normal}
+
+if [ "${mode}" == "dev" ]; then
+    venv="${ppmd}/.venv"
+
+    python3 -m venv "${venv}"
+    "$venv/bin/python3" -m ensurepip --upgrade
+    "$venv/bin/pip3" install -r requirements_dev.txt
+fi
